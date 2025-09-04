3ac82d98d90c2bf8fe05e7ddea676631a63d7a30
Ayush-Sharma12 <goodvibes121210@gmail.com>
2025-09-04 18:29:03 +0530

    9/4/25 POTD

---
+++ Easy/3516.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int findClosest(int x, int y, int z) {
+          
+           int distOfPerson1 = Math.abs(x - z);
+
+           int distOfPerson2 = Math.abs(y - z);
+
+           if(distOfPerson1 == distOfPerson2) {
+               return 0;
+           }   
+           else if(distOfPerson1 < distOfPerson2) {
+                 return 1;
+           }else{
+              return 2;
+           }
+    }
+}
