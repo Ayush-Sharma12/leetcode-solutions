768731b3f2d6ed0e20ea9e5b8180bd8bd9b7becf
Ayush-Sharma12 <goodvibes121210@gmail.com>
2025-09-12 12:23:17 +0530

    9/12/25 POTD

---
+++ Medium/3227.java
@@ -0,0 +1,9 @@
+class Solution {
+    public boolean doesAliceWin(String s) {
+          
+           
+           return s.contains("a") || s.contains("e") || s.contains("i")
+                  || s.contains("o") || s.contains("u");
+        
+    }
+}
