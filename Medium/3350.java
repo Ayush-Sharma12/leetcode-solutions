2688cf2151d630921f2d70e94ef4cc488c768c59
Ayush-Sharma12 <118461729+Ayush-Sharma12@users.noreply.github.com>
2025-10-15 20:07:52 +0530

    Update 3350.java

--- Medium/3350.java
+++ Medium/3350.java
@@ -1,3 +1,14 @@
+// Time Complexity: O(N)
+// Space Complexity: O(N)
+
+/*
+  i have used sliding window approach with a maximum size of k which is determined by inner while loop 
+  along with that i have also maintained a prevLen pointer to checkout the adjacent subarray len
+  which makes it easier identify the correct subarray, if the length of two adjacent array are not same
+  then my algorithm select the minimum one among them, and also i keep a track of currlen to use its half len
+  for possible k size. 
+*/
+
 class Solution {
     public int maxIncreasingSubarrays(List<Integer> nums) {
            
@@ -16,15 +27,15 @@ class Solution {
                      int currLen = j - i;
 
                     
-                     maxK = Math.max(maxK, currLen / 2);
+                     maxK = Math.max(maxK, currLen / 2); // possiblity, if curr len is much higher --->  then we can divide it to make two adjacent valid subarray
                      
 
                      if(prevLen != -1) {
-                         maxK = Math.max(maxK, Math.min(currLen, prevLen));
+                         maxK = Math.max(maxK, Math.min(currLen, prevLen)); // take minimum of two adjacent array if len are not same
                       }
                      
                      prevLen = currLen;
-                     i = j;
+                     i = j; // move on for the next one
              }
 
              return maxK;
